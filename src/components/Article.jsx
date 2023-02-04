882a1fdd8b8c532948d5ca526673b4e04d2e16cb
Tomasz <tomasz.iwanicki@onet.pl>
2023-02-04 11:04:38 +0100

    Complete Articles layout for desktop view

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
-const Article = () => {
-  return <div>Article</div>
+const Article = (props) => {
+  return (
+    <div className="flex flex-col justify-center w-[20%]">
+      <img className="h-[170px]" src={props.image} alt="img" />
+      <div className="px-4 mt-6">
+        <h4 className="text-grayischBlue text-xs">{props.author}</h4>
+        <h3 className="text-m mt-4 mb-2">{props.title}</h3>
+        <p className="text-grayischBlue text-sm">{props.desc}</p>
+      </div>
+    </div>
+  )
 }
 
 export default Article
--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { data } from '../constants'
+import Article from './Article'
 
 const Articles = () => {
-  return <div>Articles</div>
+  return (
+    <div className="flex flex-col bg-veryLightGray pt-16 pl-32">
+      <h2 className="text-3xl font-normal mb-8">Latest Articles</h2>
+      <div className="flex gap-8">
+        {data.articles.map((article) => (
+          <Article key={article.id} image={article.image} author={article.author} title={article.title} desc={article.desc} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Articles
