d30519d80136c7d9389b044062f005d2a5aac61a
Tomasz <tomasz.iwanicki@onet.pl>
2023-02-04 11:08:30 +0100

    Add some bottom margin

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -6,7 +6,7 @@ const Articles = () => {
   return (
     <div className="flex flex-col bg-veryLightGray pt-16 pl-32">
       <h2 className="text-3xl font-normal mb-8">Latest Articles</h2>
-      <div className="flex gap-8">
+      <div className="flex gap-8 mb-16">
         {data.articles.map((article) => (
           <Article key={article.id} image={article.image} author={article.author} title={article.title} desc={article.desc} />
         ))}
