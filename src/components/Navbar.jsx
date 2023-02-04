7e744f823f4232dd09f18d38dc82452fa48c3280
Tomasz <tomasz.iwanicki@onet.pl>
2023-02-04 15:46:13 +0100

    Create layout for both mobile and tabled devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { images } from '../constants'
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
-    <div className="container flex justify-around items-center py-4">
+    <div className="container flex justify-between px-8 sm:px-0 sm:justify-around items-center py-4 relative">
       <a href="#">
         <img src={images.logo} alt="logo" />
       </a>
-      <ul className="flex gap-8">
+      <ul className="hidden sm:flex gap-8">
         <a href="#home">
           <li className="border-b-white border-b-4 py-2 text-grayischBlue hover:border-b-limeGreen hover:cursor-pointer hover:text-black">Home</li>
         </a>
@@ -24,7 +25,29 @@ const Navbar = () => {
           <li className="border-b-white border-b-4 py-2 text-grayischBlue hover:border-b-limeGreen hover:cursor-pointer hover:text-black">Careers</li>
         </a>
       </ul>
-      <button className="custom-button">Request Invite</button>
+      <button className="hidden lg:flex custom-button">Request Invite</button>
+      <img className="flex sm:hidden" src={toggleMenu ? images.iconClose : images.iconHamburger} alt="menu" onClick={() => setToggleMenu(!toggleMenu)} />
+      {toggleMenu && (
+        <div className="absolute top-[70px] mx-auto w-[80%] rounded bg-white z-5 py-4">
+          <ul className="flex flex-col gap-6 text-2xl font-normal justify-center items-center">
+            <a href="#">
+              <li onClick={() => setToggleMenu(false)}>Home</li>
+            </a>
+            <a href="#about">
+              <li onClick={() => setToggleMenu(false)}>About</li>
+            </a>
+            <a href="#contact">
+              <li onClick={() => setToggleMenu(false)}>Contact</li>
+            </a>
+            <a href="#blog">
+              <li onClick={() => setToggleMenu(false)}>Blog</li>
+            </a>
+            <a href="#blog">
+              <li onClick={() => setToggleMenu(false)}>Careers</li>
+            </a>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
