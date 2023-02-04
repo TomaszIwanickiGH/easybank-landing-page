59528ec7b3c4a7cb152d1f310d854ec9c3de7734
Tomasz <tomasz.iwanicki@onet.pl>
2023-02-04 16:03:21 +0100

    Create layout for Benefits component for mobile and tablet devices

--- src/components/Benefit.jsx
+++ src/components/Benefit.jsx
@@ -3,10 +3,10 @@ import { images } from '../constants'
 
 const Benefit = (props) => {
   return (
-    <div className="flex flex-col justify-center  w-[20%]">
+    <div className="flex flex-col justify-center items-center lg:items-start w-full sm:w-[45%] lg:w-[20%]">
       <img className="w-[60px] h-[60px] mb-4" src={props.image} alt="img" />
       <h3 className="my-6 text-[18px]">{props.title}</h3>
-      <p className="text-grayischBlue text-sm">{props.desc}</p>
+      <p className="text-grayischBlue text-sm px-6 text-center lg:text-start lg:px-0">{props.desc}</p>
     </div>
   )
 }
--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -4,12 +4,12 @@ import Benefit from './Benefit'
 
 const Benefits = () => {
   return (
-    <div id="about" className="bg-lightGrayischBlue flex flex-col pl-32">
-      <div className="w-[50%]">
-        <h2 className="text-3xl mt-12 font-normal">Why choose Easybank?</h2>
-        <p className="text-grayischBlue my-4">We leverage Open Banking to turn your bank account into financial hub. Control your finances like never before.</p>
+    <div id="about" className="bg-lightGrayischBlue flex flex-col pl-0 lg:pl-32">
+      <div className="w-full lg:w-[50%] px-4">
+        <h2 className="text-3xl mt-12 font-normal text-center lg:text-left">Why choose Easybank?</h2>
+        <p className="text-grayischBlue my-4 text-center lg:text-start">We leverage Open Banking to turn your bank account into financial hub. Control your finances like never before.</p>
       </div>
-      <div className="flex gap-12 mt-8 mb-16">
+      <div className="flex flex-wrap gap-12 mt-8 mb-16">
         {data.benefits.map((benefit) => (
           <Benefit key={benefit.id} image={benefit.image} title={benefit.title} desc={benefit.desc} />
         ))}
