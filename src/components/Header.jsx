e00da789b8385e47a3afac7936cef8bddc5fd040
Tomasz <tomasz.iwanicki@onet.pl>
2023-02-04 15:46:46 +0100

    Create layout for both mobile and tabled devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,18 @@ import { images } from '../constants'
 
 const Header = () => {
   return (
-    <div id="home" className="container flex justify-around bg-veryLightGray relative overflow-hidden h-[800px]">
-      <div className="flex flex-col justify-center items-center]">
-        <h1 className="text-5xl w-[50%]">
+    <div id="home" className="container flex flex-col xl:flex-row justify-around bg-veryLightGray relative overflow-hidden sm:h-[1200px] h-[800px] lg:h-[800px] z-[-1]">
+      <div className="flex flex-col justify-center items-center] absolute top-[50%] sm:top-[70%]  lg:static lg:top-0">
+        <h1 className="text-4xl text-center lg:text-left lg:text-5xl lg:w-[50%]">
           Next generation <br /> digital banking
         </h1>
-        <p className="text-grayischBlue my-8 w-[40%]">Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing and much more.</p>
-        <button className="custom-button">Request Invite</button>
+        <p className="text-grayischBlue my-8 text-center px-5 lg:text-start lg:px-0 lg:w-[40%]">Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing and much more.</p>
+        <button className="custom-button mx-auto lg:mx-0">Request Invite</button>
       </div>
       <div className="container absolute right-0 top-0">
-        <img className="absolute top-[-250px] right-[-500px]" src={images.bgIntroDesktop} alt="intro" />
-        <img className="absolute top-[20px] right-[-70px] h-[800px]" src={images.imageMockups} alt="" />
+        <img className="hidden sm:flex absolute top-[-250px] right-[-500px]" src={images.bgIntroDesktop} alt="intro" />
+        <img className="flex sm:hidden absolute right-0 w-full" src={images.bgIntroMobile} alt="intro" />
+        <img className="absolute top-[-120px] lg:top-[20px] lg:right-[-70px] lg:h-[800px]" src={images.imageMockups} alt="" />
       </div>
     </div>
   )
