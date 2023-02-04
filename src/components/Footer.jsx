823dd12466639f7e9de882968622b994475e7af5
Tomasz <tomasz.iwanicki@onet.pl>
2023-02-04 16:15:24 +0100

    Complete footer for all devices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,12 @@ import { images } from '../constants'
 
 const Footer = () => {
   return (
-    <div id="contact" className="flex justify-around bg-darkBlue py-12">
+    <div id="contact" className="flex flex-col md:flex-row justify-around bg-darkBlue py-12">
       <div className="flex flex-col justify-center items-center">
         <a href="#">
           <img src={images.footerLogo} alt="logo" />
         </a>
-        <div className="flex gap-4 mt-12">
+        <div className="flex gap-4 mt-8 md:mt-12">
           <img src={images.iconFacebook} alt="facebook" />
           <img src={images.iconYoutube} alt="youtube" />
           <img src={images.iconTwitter} alt="twitter" />
@@ -16,7 +16,7 @@ const Footer = () => {
           <img src={images.iconInstagram} alt="instagram" />
         </div>
       </div>
-      <ul className="flex flex-col text-white gap-3">
+      <ul className="flex flex-col text-white gap-3 items-center md:items-start mt-8 md:mt-0">
         <a href="#about">
           <li className="hover:text-limeGreen">About Us</li>
         </a>
@@ -27,7 +27,7 @@ const Footer = () => {
           <li className="hover:text-limeGreen">Blog</li>
         </a>
       </ul>
-      <ul className="flex flex-col text-white gap-3">
+      <ul className="flex flex-col text-white gap-3 items-center md:items-start mt-3 md:mt-0">
         <a href="#blog">
           <li className="hover:text-limeGreen">Careers</li>
         </a>
@@ -38,7 +38,7 @@ const Footer = () => {
           <li className="hover:text-limeGreen">Privacy Policy</li>
         </a>
       </ul>
-      <div className="flex flex-col gap-6 items-end">
+      <div className="flex flex-col gap-6 items-center md:items-end mt-8 md:mt-0">
         <button className="custom-button">Request Invite</button>
         <p className="text-sm text-grayischBlue">&copy; Easybank. All Rights Reserved</p>
       </div>
