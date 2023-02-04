a6d9c597fe7e599998f90cc0c8adb73f95702c04
Tomasz <tomasz.iwanicki@onet.pl>
2023-02-04 10:50:07 +0100

    Complete Benefits section for desktop view

--- src/components/Benefit.jsx
+++ src/components/Benefit.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { images } from '../constants'
 
-const Benefit = () => {
-  return <div>Benefit</div>
+const Benefit = (props) => {
+  return (
+    <div className="flex flex-col justify-center  w-[20%]">
+      <img className="w-[60px] h-[60px] mb-4" src={props.image} alt="img" />
+      <h3 className="my-6 text-[18px]">{props.title}</h3>
+      <p className="text-grayischBlue text-sm">{props.desc}</p>
+    </div>
+  )
 }
 
 export default Benefit
--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { data } from '../constants'
+import Benefit from './Benefit'
 
 const Benefits = () => {
-  return <div>Benefits</div>
+  return (
+    <div className="bg-lightGrayischBlue flex flex-col pl-32">
+      <div className="w-[50%]">
+        <h2 className="text-3xl mt-12 font-normal">Why choose Easybank?</h2>
+        <p className="text-grayischBlue my-4">We leverage Open Banking to turn your bank account into financial hub. Control your finances like never before.</p>
+      </div>
+      <div className="flex gap-12 mt-8 mb-16">
+        {data.benefits.map((benefit) => (
+          <Benefit key={benefit.id} image={benefit.image} title={benefit.title} desc={benefit.desc} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Benefits
